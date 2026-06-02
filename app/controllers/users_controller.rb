9b56d108ab3e1b4e4a1e88c98bfd55ff29c9e81b
Kazuki15 <kazuki.ishikawa@kellogg.northwestern.edu>
2026-06-02 01:48:22 +0000

    2nd

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,8 +6,9 @@ class UsersController < ApplicationController
     @user = User.new
     @user["username"] = params["username"]
     @user["email"] = params["email"]
-    @user["password"] = params["password"]
+    @user.password = params["password"]
     @user.save
+    session[:user_id] = @user.id
     redirect_to "/"
   end
 end
