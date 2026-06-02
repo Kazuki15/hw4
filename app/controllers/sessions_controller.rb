8b72fae67a4f54ccde249dde85f681e01aada7fa
Kazuki15 <kazuki.ishikawa@kellogg.northwestern.edu>
2026-06-02 01:23:47 +0000

    1st modi

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,9 +3,17 @@ class SessionsController < ApplicationController
   end
 
   def create
+    user = User.find_by(email: params[:email])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id  # LogIn
+      redirect_to places_path
+    else
+      redirect_to login_path  #Failure
+    end
   end
 
   def destroy
+    session[:user_id] = nil  # LogOut
+    redirect_to login_path
   end
 end
-  
